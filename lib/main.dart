2f6a2b7d6c9b47fddd5ae3e55323bffc53bea412
R1tMah <rimahapatra7@gmail.com>
2024-04-22 19:48:01 -0400

    dick

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'poop2',
+      title: 'poop2dick',
       theme: ThemeData(
         // This is the theme of your application.
         //
