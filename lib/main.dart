931b1afe0f1fe4bdd6874331a53bf3054b141c80
nithinbhandari <nithinbhandari7@gmail.com>
2024-04-25 18:54:55 -0400

    bottom color

--- lib/main.dart
+++ lib/main.dart
@@ -117,7 +117,7 @@ class SchedulePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Schedule Page'),
+      child: Text('SchedulePage'),
     );
   }
 }
