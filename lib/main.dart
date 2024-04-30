d16e2b983ebe84caf706ff3b6e45c337880ee7b4
R1tMah <rimahapatra7@gmail.com>
2024-04-30 19:58:50 -0400

    Added the quiz functionality added comments too

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 
 
 /*
-Need to change all of the widgets to stateful widgets
-because you can actually change those, stateless widgets don't work because
-if we want user input or whatever, a stateless widget won't work for that
-I didn't feel like changing them for now because a stateful widget requires different
-functions for it. We should change them sometime. Also need to decide if we want
-the bar to be there whenever we do the quiz or do we not need it cus it doesn't transfer over
+COMMENTS FOR CHANGE:
+ADDED A QUESTION FOR THE QUIZ BUT NEED TO FIGURE OUT HOW TO STORE THE DIFFERENT
+QUESTIONS WITHIN THE APP AND NEED TO FIND A WAY TO CHANGE THE SCREEN AND SEE IF QUESTIONS ARE RIGHT
+
 */
 void main() {
   runApp(const MyApp());
