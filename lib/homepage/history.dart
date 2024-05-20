893be7c0009dd5e8173a3eca327c3e044ab899e1
adnankabir <adnankabir.va@gmail.com>
2024-05-20 11:53:16 -0400

    mission change

--- lib/homepage/history.dart
+++ lib/homepage/history.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
-class HistoryPage extends StatelessWidget {
+import 'package:table_calendar/table_calendar.dart';
+
+class HistoryPage extends StatefulWidget {
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('History Page',
-        style: TextStyle(color: Colors.white),
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFE6D4B9), // Beige color
+              Color(0xFF004D40), // Dark teal color
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay; // update `_focusedDay` here as well
+              });
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+            calendarStyle: CalendarStyle(
+              defaultTextStyle: TextStyle(color: Colors.black87), // Default day text color
+              weekendTextStyle: TextStyle(color: Colors.black87), // Weekend day text color
+              selectedTextStyle: TextStyle(color: Colors.black), // Selected day text color
+              todayTextStyle: TextStyle(color: Colors.black), // Today's day text color
+              todayDecoration: BoxDecoration(
+                color: Colors.white, // Background color for today
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Colors.blue, // Background color for selected day
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.blue, width: 2),
+              ),
+            ),
+            headerStyle: HeaderStyle(
+              titleTextStyle: TextStyle(color: Colors.black, fontSize: 16), // Title text color
+              formatButtonTextStyle: TextStyle(color: Colors.black), // Format button text color
+              formatButtonDecoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              leftChevronIcon: Icon(
+                Icons.chevron_left,
+                color: Colors.black,
+              ),
+              rightChevronIcon: Icon(
+                Icons.chevron_right,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
